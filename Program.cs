5ddad1d2ffc122721d17386878c2195b954ee6ae
Gelis <wergelis@msn.com>
2026-04-16 14:43:38 +0800

    test PR

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("測試 GitHub Copilot PR Extensions.......");
+Console.WriteLine("測試 GitHub Copilot PR Extensions......."); 
+throw new Exception("test PR");
 Console.ReadLine();
