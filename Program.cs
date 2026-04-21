21ddc21a76af8273d1a17d0bd1222da818919489
Gelis <wergelis@msn.com>
2026-04-21 19:05:03 +0800

    更新輸入提示詞，新增選擇執行測試 PR 1234 的邏輯

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
 ﻿Console.WriteLine("Hello, World!!!");
 Console.WriteLine("測試 GitHub Copilot PR Extensions.......");
-Console.WriteLine("輸入提示詞：(1:繼續;2:重新開始)");
-throw new Exception("test PR 1234");
-Console.ReadLine();
+Console.WriteLine("輸入1執行測試 PR 1234; 輸入 2 則正常結束程式。");
+
+var key = Console.ReadKey(intercept: true);
+Console.WriteLine();
+
+if (key.KeyChar == '1')
+{
+    Console.WriteLine("測試 PR 1234 的內容，這裡應該會有一些變更的程式碼。");
+}
+else if (key.KeyChar == '2')
+{
+    Console.WriteLine("正常結束程式。");
+}
